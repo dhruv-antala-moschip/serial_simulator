430b35ba37434a72fa78e6d6bdd722e1cd9acefc
dhruv-antala-moschip <dhruv.antala@moschip.com>
2025-01-08 19:09:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,42 +1,37 @@
+
 import time
 import serial
 from simulator.serial_handler import SerialHandler
 from simulator.message_validator import MessageValidator
 from simulator.logger import Logger
 from simulator.errors import SerialPortError, InvalidMessageError
-
-# Initialize components
 logger = Logger("communication.log")
 serial_handler = SerialHandler()
-# Simulated Messages
-valid_message = '{"id": 1, "content": "Hello", "timestamp": "2025-01-01T10:00:00"}'
-invalid_message = '{"id": , "content": "Invalid", "timestamp": "2025-01-01T10:00:00"}'
-
+valid_message = '{"id": 1 , "content": "Hello", "timestamp": "2025-01-01T10:00:00"}'
+invalid_message = '{"id": "one", "content": "Invalid", "timestamp": "2025-01-01T10:00:00"}'
 
 def run_simulator():
     try:
-        port_1 = "/dev/pts/15"
-        port_2 = "/dev/pts/16"
+        port_1 = "/dev/pts/7"
+        port_2 = "/dev/pts/8"
         print("Opening virtual serial ports...")
         serial_handler.open_port(port_1, 9600)
         receiver = serial.Serial(port_2, 9600, timeout=1)
-        time.sleep(1)
+        # time.sleep(1)
         print("Sending valid message...")
         try:
             if MessageValidator.validate_message(valid_message):
                 serial_handler.send_message(valid_message.encode('utf-8'))
                 logger.log_message("SEND", valid_message)
-        except InvalidMessageError as e:
-            print(e)
-        print("Simulating reception of message...")
-        receiver.write(valid_message.encode('utf-8'))
-        received_message = receiver.readline().decode('utf-8')
 
+        except InvalidMessageError :
+            print("Invalid json Format")
+        print("Simulating reception of message...")
+        received_message = receiver.readline().decode('utf-8')
+        print(received_message)
         try:
             if MessageValidator.validate_message(received_message):
-
-                logger.log_message("received"
-                                   "", received_message)
+                logger.log_message("received", received_message)
                 print(f"Received valid message: {received_message}")
         except InvalidMessageError as e:
             logger.log_message("error", str(e))
@@ -55,7 +50,5 @@ def run_simulator():
         serial_handler.close_port()
         receiver.close()
 
-
 if __name__ == "__main__":
     run_simulator()
-
